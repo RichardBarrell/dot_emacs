a03c453581e746858455f5508aaf61c19ea2fb7c
Richard Barrell <rchrd@brrll.co.uk>
2013-12-04 15:23:10 +0000

    Lorem-ipsum is gone, remove old references to it.

--- dot_emacs.el
+++ dot_emacs.el
@@ -499,13 +499,6 @@ act like (other-window -1)."
 
 (setq inhibit-startup-message t)
 
-(add-hook 'sgml-mode-hook (lambda ()
-			    (setq Lorem-ipsum-paragraph-separator "<br /><br />\n"
-				  Lorem-ipsum-sentence-separator "&nbsp;&nbsp;"
-				  Lorem-ipsum-list-beginning "<ul>\n"
-				  Lorem-ipsum-list-bullet "<li>"
-				  Lorem-ipsum-list-item-end "</li>\n"
-				  Lorem-ipsum-list-end "</ul>\n")))
 
 (add-hook 'python-mode-hook
   (lambda ()
