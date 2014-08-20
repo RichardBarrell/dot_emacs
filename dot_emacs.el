a9aaa5f0f1c8d6d9df2051fde9e1c52408ddc652
Richard Barrell <rchrd@brrll.co.uk>
2014-08-20 02:24:26 +0100

    Removing some Ruby stuff which looks bitrotten.

--- dot_emacs.el
+++ dot_emacs.el
@@ -105,25 +105,6 @@
   '(lambda() (coffee-custom)))
 
 (require 'ruby-mode)
-(require 'inf-ruby)
-(require 'ri)
-
-(setq ruby-program-name "/usr/bin/irb1.9.1 --inf-ruby-mode")
-
-(autoload 'ruby-mode "ruby-mode"
-  "Mode for editing ruby source files" t)
-(setq auto-mode-alist
-      (append '(("\\.rb$" . ruby-mode)) auto-mode-alist))
-(setq interpreter-mode-alist (append '(("ruby" . ruby-mode))
-				     interpreter-mode-alist))
-(autoload 'run-ruby "inf-ruby"
-  "Run an inferior Ruby process")
-(autoload 'inf-ruby-keys "inf-ruby"
-  "Set local key defs for inf-ruby in ruby-mode")
-(add-hook 'ruby-mode-hook
-      '(lambda ()
-         (inf-ruby-keys)
-))
 
 (require 'puppet-mode)
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
