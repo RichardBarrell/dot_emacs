36ed82f4db25458716a2b99c1e6068b9e9d2dd76
Richard Barrell <rchrd@brrll.co.uk>
2015-02-16 13:37:29 +0000

    stupid typo

--- dot_emacs.el
+++ dot_emacs.el
@@ -341,9 +341,9 @@
 
 (defun my-js2-mode-hook ()
   (require 'espresso)
-  (setq espresso-indent-level 4
-        indent-tabs-mode nil
-        c-basic-offset 4)
+  (setq espresso-indent-level 8
+        indent-tabs-mode 't
+        c-basic-offset 8)
   (c-toggle-auto-state 0)
   (c-toggle-hungry-state 1)
   (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
